ce6b906ac5596ddf47ff4f0c6c8b5646b2ddbce2
goldenstein64 <jamesramsauer@gmail.com>
2021-11-29 02:31:33 -0500

    exposed fxml to more artists

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,7 +6,13 @@ module venusians {
   opens venusians.gui.main to javafx.fxml;
   opens venusians.gui.results to javafx.fxml;
   opens venusians.gui.setup to javafx.fxml;
+  opens venusians.gui.main.artists.tradeRequest to javafx.fxml;
+  opens venusians.gui.main.artists.tradeDraft to javafx.fxml;
+  opens venusians.gui.main.artists.resourceChoice to javafx.fxml;
+  opens venusians.gui.main.artists.resourceMultiChoice to javafx.fxml;
+  opens venusians.gui.main.artists.developmentToolTip to javafx.fxml;
 
+  opens venusians.data to javafx.base;
   opens venusians.data.lifecycle to javafx.base;
   opens venusians.data.cards.resource to javafx.base;
   opens venusians.data.board.tiles to javafx.base;
