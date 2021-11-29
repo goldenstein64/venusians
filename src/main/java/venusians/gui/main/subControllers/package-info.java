8973863365881572fabad12eae018a1467fcc798
goldenstein64 <jamesramsauer@gmail.com>
2021-11-29 02:08:28 -0500

    Added some beginning package documentation

---
+++ src/main/java/venusians/gui/main/subControllers/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Defines a collection of controllers that take control from the main game
+ * controller and give it back after a tail call.
+ */
+package venusians.gui.main.subControllers;
---
+++ src/main/java/venusians/gui/main/windows/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Defines a collection of modal windows that appear in front of the main game
+ * view. These are usually accompanied by a frontdrop and a button state of
+ * MODAL.
+ *
+ * Windows are technically a type of sub-controller, as they take control and
+ * give it back using a tail call.
+ */
+package venusians.gui.main.windows;
