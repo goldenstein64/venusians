ef228726400b2900bce99156d8e67cee5d18a852
goldenstein64 <jamesramsauer@gmail.com>
2021-11-29 02:11:47 -0500

    class Buildable now has a non-static `getBlueprint()` method
    By convention, any classes that implement this should have a static `BLUEPRINT` constant.

--- src/main/java/venusians/data/board/buildable/Buildable.java
+++ src/main/java/venusians/data/board/buildable/Buildable.java
@@ -1,15 +1,10 @@
 package venusians.data.board.buildable;
 
-import venusians.data.board.IntPoint;
 import venusians.data.cards.resource.ResourceCardMap;
 import venusians.data.players.Player;
 
 public interface Buildable {
-  public static ResourceCardMap getBlueprint() {
-    return null;
-  }
-
-  public IntPoint getPosition();
+  public ResourceCardMap getBlueprint();
 
   public Player getOwner();
 }
